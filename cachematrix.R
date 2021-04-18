efab3e183e3fbdd38b4c5ba92c2de7be7b712833
Luis Pedraza <luispedraza@gmail.com>
2021-04-18 17:04:37 +0200

    finished the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is the assignment for weeb 3 of Coursera R Programming course
+## This file contains two functions:
 
-## Write a short comment describing this function
+## This function creates a special "matrix" object that cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL # this is the inverse matrix value
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    
+    get <- function() x
+    setInverse <- function(inverseValue) inverse <<- inverseValue
+    getInverse <- function() inverse
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix x, which is the parameter
+## If the inverse has been previously computed, returns the cached value
+## Else, computes the inverse and caches the value inside the special matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    value <- x$getInverse()
+    if (!is.null(value)) {
+        message("getting cached data")
+        return (value)
+    }
+    
+    # Else, we need to compute the inverse value:
+    theMatrix <- x$get()
+    value <- solve(theMatrix)
+    x$setInverse(value)
+    # And finally, return the inverse value:
+    value
 }
+
+## This is for testing:
+m <- matrix(c(2, 0, 0, 0, 2, 0, 0, 0, 2), 3, 3)
+print(m)
+specialM <- makeCacheMatrix(m)
+
+inverse <- cacheSolve(specialM)
+print(inverse)
+# Now, cached version
+inverse <- cacheSolve(specialM)
+print(inverse)
